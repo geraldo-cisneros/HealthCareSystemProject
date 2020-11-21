ad8aeb32c475f7c2ad6b7e1adc939e2c3105f673
Geraldo Cisneros <geraldo.a.cisneros@nasa.gov>
2020-11-21 11:52:07 -0600

    First Commit

--- src/main/java/HealthCareSystem/CancelAppointment.java
+++ src/main/java/HealthCareSystem/CancelAppointment.java
@@ -32,7 +32,6 @@ public class CancelAppointment extends javax.swing.JFrame {
         docList.addItem("C, Loya");
         docList.addItem("D, Skinner");
         docList.addItem("K, Wang");
-        
     }
 
     /**
