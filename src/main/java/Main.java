98c024384674d209a64d113714b3037d2181cab2
Geraldo Cisneros <geraldo.a.cisneros@nasa.gov>
2020-11-21 10:56:16 -0600

    First Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author geraldocisneros
+ */
+public class Main {
+    
+}
+
+class checkCredentials {
+    
+}
+
+class writeToFile{
+    static void writeFile(String f){
+        
+    }
+}
